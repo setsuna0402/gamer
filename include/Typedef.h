777be03ef55da11ecb90890ca97b30b2fa3fcbb9
setsuna0402 <mayuyuqueen@gmail.com>
2024-01-02 14:21:30 +0800

    fixed bug

--- include/Typedef.h
+++ include/Typedef.h
@@ -56,7 +56,7 @@ const TestProbID_t
    TESTPROB_HYDRO_BARRED_POT                   =   51,
    TESTPROB_HYDRO_CDM_LSS                      =  100,
    TESTPROB_HYDRO_ZELDOVICH                    =  101,
-   TESTPROB_ELBDM_EXTPOT                       = 1000;
+   TESTPROB_ELBDM_EXTPOT                       = 1000,
    TESTPROB_RT_UNIFORM_RHO                     = 2000;
 
 
